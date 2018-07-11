87e3e05d3481210efad72a95f44adf84f27c6ac2
Helmi Agustian <helmi.agustian@bukalapak.com>
2018-07-11 14:39:21 +0700

    Update comment

--- main.go
+++ main.go
@@ -5,20 +5,20 @@ import (
 )
 
 func tozero(z *int) {
-	*z = 0
+	*z = 0 // assign value 0 to memory that have address value of z
 }
 
 func main() {
 	i := 1
-	fmt.Printf("%v\n", i)
-	fmt.Println(&i)
+	fmt.Printf("%v\n", i) // print variable i value
+	fmt.Println(&i)       // print variable i address value
 
-	var j *int
-	j = &i
-	fmt.Println(j)
+	var j *int     // create pointer variable j
+	j = &i         // variable j now have variable i address value
+	fmt.Println(j) // print variable j value
 
-	*j = 2
-	fmt.Println(i)
+	*j = 2         // assign value 2 to memory that have address value of j
+	fmt.Println(i) // print variable i value
 
 	tozero(j)
 	fmt.Println(i)
